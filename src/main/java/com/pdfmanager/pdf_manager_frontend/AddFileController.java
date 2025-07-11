b5568ed495eef9ceec9db82a4a6e2b926951a0de
FilipeFCampos <filipefcampos2005@gmail.com>
2025-07-11 11:41:31 -0300

    Working on the addfile functionality

--- src/main/java/com/pdfmanager/pdf_manager_frontend/AddFileController.java
+++ src/main/java/com/pdfmanager/pdf_manager_frontend/AddFileController.java
@@ -2,14 +2,16 @@ package com.pdfmanager.pdf_manager_frontend;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 public class AddFileController {
     @FXML RadioButton bookrb;
@@ -18,6 +20,11 @@ public class AddFileController {
     @FXML TextField titlefield;
     @FXML TextField pathfield;
     @FXML TextField authorsfield;
+    @FXML StackPane stackPane;
+    @FXML TextField fieldofknowledgefield;
+    @FXML TextField subtitlefield;
+    @FXML TextField publisherfield;
+    @FXML TextField publishyearfield;
     Map<String, String> buffer = new HashMap<>();
     @FXML public Text output;
 
@@ -45,9 +52,49 @@ public class AddFileController {
 
         buffer.put("title", title);
         buffer.put("authors", authors);
+        buffer.put("path", path);
         buffer.put("type", type);
+
         printToGUI("deu bom");
+        try {
+            disablePane("addFileGlobal");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        if (type.equals("book")) {
+
+        }
         // Send to backend logic here
+    }
+
+    private String addBook() {
+        String subtitle = subtitlefield.getText();
+        String fieldofknowledge = fieldofknowledgefield.getText();
+        String publisher = publisherfield.getText();
+        String publishyear = publishyearfield.getText();
+
+        if (subtitle.isEmpty() || fieldofknowledge.isEmpty() || publishyear.isEmpty() || publisher.isEmpty()) {
+            showAlert("Please fill all fields.");
+            return null;
+        }
+
+        return null;
+    }
+
+    private void disablePane(String id) throws IOException {
+        Node node = stackPane.lookup(id);
+        if (node != null) {
+            node.setDisable(true);
+            node.setVisible(false);
+        }
+    }
+
+    private void enablePane(String id) throws IOException {
+        Node node = stackPane.lookup(id);
+        if (node != null) {
+            node.setDisable(false);
+            node.setVisible(true);
+        }
     }
 
     private void showAlert(String message) {
